110cd1a262a8a7b6391f1651038e96a022c2ace7
Mahe Tardy <mahe.tardy@gmail.com>
2023-05-30 12:51:57 +0200

    Modify install/k8s/test.sh to be portable and add postUpgradeCommand

--- install/kubernetes/test.sh
+++ install/kubernetes/test.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
 set -e
+set -o pipefail
 shopt -s expand_aliases
 
+# make this script executable from anywhere
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+cd $SCRIPT_DIR
+
 alias helm='docker run --rm -v $(pwd):/apps alpine/helm:3.3.4'
+alias kubeval='docker run --rm -i garethr/kubeval:0.15.0'
 helm dependency update .
 helm lint . --with-subcharts
 helm template tetragon . | kubeval --strict --additional-schema-locations https://raw.githubusercontent.com/joshuaspence/kubernetes-json-schema/master
